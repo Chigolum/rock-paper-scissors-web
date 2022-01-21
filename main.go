a84c62d4ad89e4366f297b2b4c5f4ad49a1c4961
Chigolum Obikile <chigolum.obikile@interswitchgroup.com>
2022-01-21 23:43:14 +0100

    using json to parse response to browser

--- main.go
+++ main.go
@@ -1,20 +1,46 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"log"
+	"myapp/rps"
 	"net/http"
+	"text/template"
 )
 
 func homePage(w http.ResponseWriter, r *http.Request) {
-	html := `<strong>Hello, world</strong>`
-	w.Header().Set("Content-Type", "text/html")
-	//Prints to the response writer
-	fmt.Fprint(w, html)
+	renderTemplate(w, "index.html")
+}
+
+func playRound(w http.ResponseWriter, r *http.Request) {
+	result := rps.PlayRound(1)
+	out, err := json.MarshalIndent(result, "", "    ")
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(out)
 }
 
 func main() {
+	http.HandleFunc("/play", playRound)
 	http.HandleFunc("/", homePage)
 	log.Println("Starting web server on port 8080")
 	http.ListenAndServe(":8080", nil)
+}
+
+func renderTemplate(w http.ResponseWriter, page string) {
+	t, err := template.ParseFiles(page)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	err = t.Execute(w, nil)
+
+	if err != nil {
+		log.Println(err)
+		return
+	}
 }
